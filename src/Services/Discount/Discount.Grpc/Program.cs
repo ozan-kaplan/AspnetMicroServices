3c40566dbcd668c38a98a0c6b5370565f3026fa9
ozan-kaplan <ozan.kaplan@outlook.com>
2023-10-27 11:48:40 +0300

    Discount.Grpc project developed.

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,3 +1,5 @@
+using Discount.Grpc.Extensions;
+using Discount.Grpc.Repositories;
 using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,10 +10,22 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddGrpc();
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+
 var app = builder.Build();
 
+
+app.MigrateDatabase<Program>();  
+
+
+
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+ app.MapGrpcService<DiscountService>();
+
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
